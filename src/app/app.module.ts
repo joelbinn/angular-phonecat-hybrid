bcee2b8da8a672b1bb2f0fdd8c2d058cb4009a8e
Joel Binnquist <joel.binnquist@gmail.com>
2017-05-06 10:25:42 +0200

    Completed integrating AngularJS application as hybrid. Now with Angular root component phonecat-app.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { UpgradeModule } from '@angular/upgrade/static';
+import { PhonecatAppComponent } from './phonecat-app.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     UpgradeModule
+  ],
+  declarations:[
+    PhonecatAppComponent
+  ],
+  bootstrap:[
+    PhonecatAppComponent
   ]
 })
 export class AppModule {
-  ngDoBootstrap() {}
 }
---
+++ src/app/phonecat-app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'phonecat-app',
+  templateUrl: './phonecat-app.component.html'
+})
+export class PhonecatAppComponent {
+
+}
